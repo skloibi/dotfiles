f48b6f4c6995ba8d2ea7ce1f08efad11d6e26949
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2021-10-21 11:49:12 +0200

    [Polybar] List all playing audio sources

--- polybar/.config/polybar/player.sh
+++ polybar/.config/polybar/player.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 # taken from https://github.com/rbnis/dotfiles/blob/dfd6f956f6d00a1012a3a167d947773095dac7fd/.config/waybar/modules/spotify.sh
 
-class=$(playerctl metadata --format '{{lc(status)}}')
 
-if [[ $class == "playing" ]]; then
-  info=$(playerctl metadata --format '{{artist}} - {{title}}')
-  if [[ ${#info} -gt 50 ]]; then
-    info=$(echo $info | cut -c1-50)"..."
+players=$(playerctl -l)
+
+text=""
+while IFS= read -r player; do
+  class=$(playerctl -p "$player" metadata --format '{{lc(status)}}')
+
+  if [[ $class == "playing" ]]; then
+    info=$(playerctl -p "$player" metadata --format '{{artist}} - {{title}}')
+    if [[ ${#info} -gt 50 ]]; then
+      info=$(echo $info | cut -c1-50)"..."
+    fi
+    text="$info $text"
+#  elif [[ $class == "paused" ]]; then
+#    text=""
+#  elif [[ $class == "stopped" ]]; then
+#    text=""
   fi
-  text=$info
-elif [[ $class == "paused" ]]; then
-  text=""
-elif [[ $class == "stopped" ]]; then
-  text=""
-fi
+
+done <<< "$players"
 
 echo -e "$text"
 
