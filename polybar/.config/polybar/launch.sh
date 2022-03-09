e25d64308de9f7631cf47a11b53900cf889e9d31
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2022-03-09 08:42:38 +0100

    [Polybar] Update launch script and add default config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,14 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+#while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar main
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar --reload main &
+  echo "---" | tee -a "/tmp/polybar${m}.log"
+  MONITOR=$m polybar --config=$HOME/.config/polybar/config.ini main 2>&1 | tee -a "/tmp/polybar${m}.log" & disown
 done
 
-echo "Polybar launched..."
+echo "Bars launched..."
